7fa420c278788364a104cb9f5f39322677ab8a22
Josh <yono@joshono.net>
2022-06-01 21:52:56 -0500

    sign in->email送信->email確認画面->ログインが正しく動作

--- users/forms.py
+++ users/forms.py
@@ -71,11 +71,3 @@ class UpdateForm(SignupForm):
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
 #from django.contrib.auth.models import User
-
-@receiver(pre_save, sender=CustomUser)
-def set_new_user_inactive(sender, instance, **kwargs):
-    if instance._state.adding is True:
-        print("Creating Inactive User")
-        instance.is_active = False
-    else:
-        print("Updating User Record")
